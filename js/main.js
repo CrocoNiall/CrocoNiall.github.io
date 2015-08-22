92ae942b8b6abdc197ddefe13f29de4ebfec1b7c
Niall Wallace <nj.wallace@outlook.com>
2015-08-22 20:33:24 +0100

    created the throwRandomCow() function to randomly select a cow and throw it with a random degree

--- js/main.js
+++ js/main.js
@@ -6,48 +6,77 @@ $(document).ready(function(){
 
 })
 
-function throwRandomCow() {
+function throwRandomCow(speed, distance) {
+  //creates two random values(degree of cow, cowIndex) and passes them to the throwCow() function. 
+  //takes 2 arguments
+  //**speed of throw
+  //**distance of throw
 
+  randomCow = Math.floor((Math.random() * 6) + 1);
+  randomDegree = Math.floor((Math.random() * 360) + 1);
+  console.log(randomDegree);
+  console.log(randomCow);
+  throwCow(randomCow, speed, randomDegree, distance);
 }
 
 
 function throwCow(cowIndex, speed, degree, distance){
-  // takes three arguments, speed of throw, angle of rotation of cow and distance of throw
+  // takes four arguments:
+  // **Cow Index
+  // **Speed of throw
+  // **Angle of cow
+  // **Distance of throw
 
   //creates three animations, one to throw cow up, one to bring cow down and the other to reset cow to origional position.
-  var div
 
+  var div
   var cowThrowHeight = '+=' + distance;
-  var reset = distance * 2;
-  var resetBottom = '-=' + reset;
   var cowThrowDist = distance;
 
   //define cow to perform function on 
   switch (cowIndex){
     case 1:
       div = $('#cow1');
+  var reset = distance * 2;
+  var resetBottom = '-=' + reset;
+  var cowThrowDist = '+=' + distance;
       break;
     case 2:
       div = $('#cow2');
+        var reset = distance * 2;
+        var resetBottom = '-=' + reset;
+        var cowThrowDist = '+=' + distance;
       break;
     case 3:
       div = $('#cow3');
+        var reset = distance * 2;
+        var resetBottom = '+=' + reset;
+        var cowThrowDist = '+=' + distance;
       break;
     case 4:
       div = $('#cow4');
       distance = distance - (distance * 2)
+        var reset = distance;
+        var resetBottom = '+=' + reset;
+        var cowThrowDist = '+=' + distance;
       break;
     case 5:
       div = $('#cow5');
       distance = distance - (distance * 2)
+        var reset = distance;
+        var resetBottom = '+=' + reset;
+        var cowThrowDist = '+=' + distance;
       break;
     case 6:
       div = $('#cow6');
       distance = distance - (distance * 2)
+        var reset = distance;
+        var resetBottom = '-=' + reset;
+        var cowThrowDist = '+=' + distance;
       break;
   }
-
-
+  var reset = distance * 2;
+  var resetBottom = '-=' + reset;
   var cowThrowDist = '+=' + distance;
 
   div.css({ WebkitTransform: 'rotate(' + degree + 'deg)'});
