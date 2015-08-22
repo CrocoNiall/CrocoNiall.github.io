03290130d2d3b150486c110d5801be96703ce6e8
Niall Wallace <nj.wallace@outlook.com>
2015-08-22 19:11:41 +0100

    compleate throwCow() function

--- js/main.js
+++ js/main.js
@@ -3,27 +3,33 @@ console.log('Hello Niall');
 
 
 $(document).ready(function(){
-  
 
-
-
- 
 })
-function throwCowUp(speed){
-    var div = $("#cow1");
+
+function throwCow(cow, speed, degree, distance){
+  // takes three arguments, speed of throw, angle of rotation of cow and distance of throw
+
+  //creates three animations, one to throw cow up, one to bring cow down and the other to reset cow to origional position. 
+  var element = '"#cow' + cow + '"';
+  console.log(cow);
+  var cowThrowDist = '+=' + distance;
+  var cowThrowHeight = '+=' + distance;
+  var reset = distance * 2;
+  var resetBottom = '-=' + reset;
+
+  //console.log(resetBottom);
+  //console.log(cowThrowDist);
+  var div = $("#cow1");
+  div.css({ WebkitTransform: 'rotate(' + degree + 'deg)'});
+  div.animate({
+    left: cowThrowDist, 
+    bottom: "+=400" }, speed, 'linear');
+
     div.animate({
-      left: "+=300", 
-      bottom: "+=350",
-      rotate: "180"}, 1000, 'linear');
+    left: cowThrowDist,
+    bottom: "-=400" },speed, 'linear');
 
-    throwCowDown();
-   } 
-
-function throwCowDown(){
-    var div = $("#cow1");
     div.animate({
-      left: "+=300", 
-      bottom: "-=350",
-      rotate: "180"},1000, 'linear');
+      left: resetBottom }, 1, 'linear');
 
-   } 
+} 
