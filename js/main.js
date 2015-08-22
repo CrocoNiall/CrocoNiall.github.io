110558c3e00b2c3ac3bd212ea9f3315a3b8d02d5
Niall Wallace <nj.wallace@outlook.com>
2015-08-22 17:54:07 +0100

    creating cowJump function()

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 console.log('Hello Niall');
+
+
+
+$(document).ready(function(){
+  
+
+
+
+ 
+})
+function throwCowUp(speed){
+    var div = $("#cow1");
+    div.animate({
+      left: "+=300", 
+      bottom: "+=350",
+      rotate: "180"}, 1000, 'linear');
+
+    throwCowDown();
+   } 
+
+function throwCowDown(){
+    var div = $("#cow1");
+    div.animate({
+      left: "+=300", 
+      bottom: "-=350",
+      rotate: "180"},1000, 'linear');
+
+   } 
